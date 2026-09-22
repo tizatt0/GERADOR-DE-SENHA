3414fcf308842507628739ca7eceee06b5da110c
tizatt0 <joao.tizatto@escola.pr.gov.br>
2026-09-22 11:32:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -61,3 +61,67 @@ senha = senha + alfabeto[numeroAleatorio];
 campoSenha.value = senha;
 classificaSenha(alfabeto.length);
 }
+}
+campoSenha.value = senha;
+
+
+
+classificaSenha(alfabeto.length);
+
+campoSenha.value = senha;
+
+
+   
+classificaSenha(alfabeto.length);
+
+function classificaSenha(tamanhoAlfabeto) {
+
+
+    let entropia =
+tamanhoSenha * Math.log2(tamanhoAlfabeto);
+
+
+   
+console.log(entropia);
+
+
+   
+forcaSenha.classList.remove('fraca', 'media', 'forte');
+
+
+    if (entropia >
+57) {
+
+
+       
+forcaSenha.classList.add('forte');
+
+
+    } else if
+(entropia > 35 && entropia < 57) {
+
+
+       
+forcaSenha.classList.add('media');
+
+
+    } else if
+(entropia <= 35) {
+
+
+       
+forcaSenha.classList.add('fraca');
+
+
+    }
+
+
+    const
+valorEntropia = document.querySelector('.entropia');
+
+
+   
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha.";
+
+
+}
